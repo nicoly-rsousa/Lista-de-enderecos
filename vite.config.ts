9ce2c8603e210d7919c7aeb341af8485555ef8b4
Nicoly <nicolytt>
2026-02-11 19:00:31 -0300

    fead-correcao

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
- base: '/https://nicoly-rsousa.github.io/Lista-de-enderecos/', 
+ base: '/Lista-de-enderecos/', 
 });
